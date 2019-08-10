796f7100ab8f0926326dd3d9bcda5d66e00fe863
halimtuhu <halimtuhuprasetyo@gmail.com>
2019-08-11 02:13:57 +0700

    add forgot password feature for user

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,22 +1,71 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ config('app.locale', 'en') }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <title>RESU-SDI | Forgot Password</title>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
 
-                            <div class="col-md-6">
+    <style>
+        html {
+            margin: 0px;
+            height: 100%;
+            width: 100%;
+        }
+
+        body {
+            margin: 0px;
+            min-height: 100%;
+            width: 100%;
+            background-color: gray;
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
+        body .container {
+            width: 100%;
+            height: 100vh;
+        }
+
+        body .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.75);
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row h-100 justify-content-center">
+            <div class="col-12 col-md-8 col-lg-6 my-auto">
+                <div class="card card-login mx-auto mt-5 border-0 shadow-sm">
+                    <div class="card-header bg-danger border-0 text-light text-center">
+                        <h3>WELCOME</h3>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
+    
+                            <input type="hidden" name="token" value="{{ $token }}">
+    
+                            <div class="form-group">
+                                <label for="email" class="text-md-right">{{ __('E-Mail Address') }}</label>
+    
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -25,12 +74,10 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
+    
+                            <div class="form-group">
+                                <label for="password" class="text-md-right">{{ __('Password') }}</label>
+    
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -39,27 +86,33 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
+    
+                            <div class="form-group">
+                                <label for="password-confirm" class="text-md-right">{{ __('Confirm Password') }}</label>
+    
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
+    
+                            <div class="form-group mb-0">
+                                <div class="offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Reset Password') }}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
+                <p class="text-center text-white small mt-5">RESU © 2019 Project SDI - By Eggie Dear Asmara - Universitas Muhammadiyah Sidoarjo</p>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('js/core/jquery.3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/core/popper.min.js') }}"></script>
+
+</body>
+
+</html>
